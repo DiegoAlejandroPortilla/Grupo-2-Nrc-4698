7cb0274f194a3be2ebba33a3cf3fa7184ef26369
DiegoAlejandroPortilla <daportilla1@espe.edu.ec>
2022-05-17 20:48:14 -0500

    Descarga

---
+++ PARCIAL I/DEBER 2/Conversion/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <cstring>
+#include <conio.h>
+using namespace std;
+	char ingresoDatos(char);
+	char *ingresoDatosEntero(char *msj){
+	char *dato=new char[10],c;
+	int i=0;
+	printf("%s ",msj);
+	while((c=getch())!=13 && (c=getch())!=8 && (c=getch())!=127){
+		if(c>='0'&& c<='9'){
+			printf("%c",c);
+			dato[i++]=c;
+		}
+	}
+	dato[i]='\0';
+	return dato;
+}
+
+int main(int argc, char** argv) {
+	char dato[10];
+	int valor=0;
+	strcpy(dato,ingresoDatosEntero("Ingrese el dato"));
+	valor=atoi(dato);
+	valor=valor*2;
+	printf("\nresultado %d......",valor);
+	return 0;
+}
